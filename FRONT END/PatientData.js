9726b6afe9d462baa78b3b2237ef3d28541c78c3
EvaNtova <eva341nt@gmail.com>
2024-09-22 19:16:28 +0300

    Delete FRONT END directory

--- FRONT END/PatientData.js
+++
@@ -1,8 +0,0 @@
-function getPatientData(patientId){
-    const url = "/patient_details";
-    const dataToSend = {
-        "patientId": patientId
-    }
-    return fetchApi(ulr,"POST",dataToSend);
-
-}
